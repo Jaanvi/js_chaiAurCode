c0df60d4767819ad6290292d019f2b80386be570
Jaanvi tiwari <139463623+Jaanvi@users.noreply.github.com>
2024-10-14 22:30:32 +0000

    datatypes

---
+++ 01_basics/02_Datatype.js
@@ -0,0 +1,26 @@
+"user strict"; //treat all js code as newer version 
+
+//alert(3+3)//yha ye aise nhi chalega becouse we are using nodejs , it works on crom and popup the output
+
+//console.log(3+3); console.log("jaanvi") it will run but the code is not redable
+
+//datatypes in js;
+let name="jaanvi"//string
+let age=20//integer
+let isgirl=true//boolean
+
+//number=>2 to power 53
+//bigint=>to store large integers
+//string=>""
+//boolean=> true/false
+//null=> in js it is a standalone value or datatype.it is empty
+//undefined=> means abhi variable ki value assign nhi hui hai eg:let name;
+//symbol=>used for uniqueness
+
+
+//object
+console.log(typeof 23)//datatype number
+console.log(typeof age)//age is in number datatype
+console.log(typeof null)//null ka type js me object hota hai na ki null 
+console.log(typeof undefined)//but undefined ke type undefined hi hota hai unlike null
+
